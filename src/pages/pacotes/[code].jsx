f125f0c08f00558d61e4e63213337959bdcde4db
Dievano Dantas <dievano_dantas@hotmail.com>
2022-06-17 00:10:05 -0300

    Bug fix - erros de tipografia next

--- src/pages/pacotes/[code].jsx
+++ src/pages/pacotes/[code].jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Router from 'next/router';
+import Link from 'next/link'
 import axios from 'axios';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -100,13 +101,16 @@ class TrackInfo extends Component {
                             <h4>Seu Pedido: {code_track}</h4>
                         </div>
                         <div className={styles.section_back}>
-                            <a href="/" className={styles.link_back}>
-                                {/* <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-                                </svg> */}
-                                <i class="bi bi-arrow-left"></i>
+                            <Link href="/">
+                                <a className={styles.link_back}>
+                                <i className="bi bi-arrow-left"></i>
                                 <span className='ms-1'>Voltar</span>
-                            </a>
+                                </a>
+                            </Link>
+                            {/* <a href="/" className={styles.link_back}>
+                                <i className="bi bi-arrow-left"></i>
+                                <span className='ms-1'>Voltar</span>
+                            </a> */}
                         </div>
                         {(info.length !== 0) ?
                             info.map((element, index) => (
