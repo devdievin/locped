2ead8d3dba98af9c09a737594904f3d3185a9510
Dievano Dantas <dievano_dantas@hotmail.com>
2022-06-19 18:06:26 -0300

    Bug Fix #1 - 19/06/2022

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -8,8 +8,8 @@ const HeadComponent = (props) => (
         <link rel="icon" href="/favicon.ico" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;0,600;1,200;1,400;1,500;1,600&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;0,600;1,200;1,400;1,500;1,600&display=swap" rel="stylesheet" />
     </Head>
 );
 
