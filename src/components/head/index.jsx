0afbbaf043cce23d55333e9827a67b728816cb3f
Dievano Dantas <dievano_dantas@hotmail.com>
2022-09-05 20:43:50 -0300

    remoção de códigos não usados

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -1,19 +1,8 @@
 import Head from "next/head";
 import React from 'react';
-// import Script from 'next/script';
 
 const HeadComponent = (props) => (
     <Head>
-        {/* <Script id="google-tag-manager" strategy="afterInteractive">
-            {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WPKSDS9');
-            `}
-        </Script> */}
-
         <title>{props.title}</title>
         <meta name="description" content="Site para rastrear encomendas" />
         <link rel="icon" href="/favicon.ico" />
