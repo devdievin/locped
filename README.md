77552566b73f12bdccd2ad1278393015919ca6aa
Dievano Dantas <dievano_dantas@hotmail.com>
2022-10-28 12:10:05 -0300

    readme.md versão 3

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ Locped - localização de pedidos é uma aplicação Web para consulta e acompan
 
 Veja a aplicação em funcionamento aqui: https://locped.vercel.app/
 
-![Página inicial](public/images/screenshots/screenshot1.png "Página inicial")
+![Página inicial](public/images/screenshots/screen0.png "Página inicial")
 
 ## Sobre
 
-O LOCPED começou como um projeto Hobbie para estudo e pela necessidade de um sistema de acompanhamento de entregas personalizado de interesse pessoal.
+O Locped surgiu como um projeto hobbie e pela necessidade de um sistema de acompanhamento de entregas personalizado de interesse pessoal.
 Depois das primeiras linhas de código, horas de pesquisas e prototipação. O projeto foi ganhando forma e gerando motivação para melhorá-lo constantemente. Atualmente realiza o acompanhamento de entregas apenas dos Correios.
 
 ## Features
 
-- Layout totalmente responsivo
 - Acompanhamento das entregas feitas pelos Correios
 - Linha do tempo com as atualizações da entrega
+- Layout totalmente responsivo
 - Compartilhe rapidamente o link no e-mail
 - Compartilhe rapidamente o link no whatsapp
 - Pré-implementação de PWA
@@ -25,14 +25,19 @@ Depois das primeiras linhas de código, horas de pesquisas e prototipação. O p
 
 Insira o código de rastreio (de 13 caracteres) no campo de pesquisa e clique em Rastrear.
 
-Na tela seguinte você verá os dados relacionados a sua entrega. Como:
+>Se você não tem um código de rastreio, pode usar este para testar: 
+```
+LB749365748HK
+```
+
+Na tela seguinte você verá os dados relacionados a sua entrega, como:
 - Código de rastreio
 - Label de status da entrega (muda a cor de acordo com o status)
 - Linha do tempo com as atualizações do pacote
 
 Você tem a possibilidade de compartilhar rapidamente o seu link de rastreio para quem você quiser. Tanto por e-mail, quanto pelo whatsapp.
 
-<img src="public/images/screenshots/screenshot2.png" alt="página inicial mobile" title="página inicial mobile" width="250" height="auto"/>
+![Páginas mobile](public/images/screenshots/mobile-screens.png "Páginas mobile Locped")
 
 ## Tecnologias
 
--- src/pages/pacotes/[code].jsx
+++ src/pages/pacotes/[code].jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import axios from 'axios';
+import { returnMsgEmail, returnMsgWhats, getImageSrc } from '../../utils/helpers';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
@@ -19,7 +20,6 @@ import ErrorPageComponent from '../../components/error';
 import FooterComponent from '../../components/footer';
 
 import styles from './Track.module.css';
-import { returnMsgEmail, returnMsgWhats } from '../../utils/helpers';
 
 class TrackInfo extends Component {
    constructor(props) {
@@ -41,222 +41,178 @@ class TrackInfo extends Component {
    }
 
    async componentDidMount() {
-   let response;
+      let response;
 
-   await axios.get(`/api/pacotes/${this.props.code}`)
-      .then(resp => { this.setConnectionTimeout(); response = resp; })
-      .catch(err => console.error(err))
-      .finally(() => this.setState({ isLoading: false }));
+      await axios.get(`/api/pacotes/${this.props.code}`)
+         .then(resp => { this.setConnectionTimeout(); response = resp; })
+         .catch(err => console.error(err))
+         .finally(() => this.setState({ isLoading: false }));
 
-   this.setState({ success: this.errorChecking(response), code_track: this.checkEventos(response.data.result.codigo), info: this.checkEventos(response.data.result.eventos) });
+      this.setState({ success: this.errorChecking(response), code_track: this.checkEventos(response.data.result.codigo), info: this.checkEventos(response.data.result.eventos) });
 
-   window.addEventListener("scroll", () => ((window.scrollY > window.screen.height) ? this.setState({ showBtnTop: true }) : this.setState({ showBtnTop: false })));
-}
-
-// Timeout de resposta da api
-setConnectionTimeout = () => {
-   const timeout = 25;
-   let st = setTimeout(() => {
-      const { isLoading } = this.state;
-      if (isLoading) {
-         this.setState({ success: 'Timeout', isLoading: false });
-      }
-      clearTimeout(st);
-   }, (timeout * 1000));
-}
-
-// Verifica se há informações dos eventos de entrega
-checkEventos = (data) => ((!data || data === null || data === "") ? null : data);
-
-// Verifica e formata as informações sobre o endereço onde está o pedido
-checkAddress = (city, uf) => ((city === undefined || uf === undefined) ? 'Brasil' : `${city}/${uf}`);
-
-// Verificação de erros na resposta dos dados
-errorChecking = (response) => {
-   if (response.data.success === false) {
-      return 'Serviço indisponível';
+      window.addEventListener("scroll", () => ((window.scrollY > window.screen.height) ? this.setState({ showBtnTop: true }) : this.setState({ showBtnTop: false })));
    }
 
-   if (response.data.result.mensagem === 'SRO-019: Objeto inválido') {
-      return 'Objeto inválido';
-   } else if (response.data.result.mensagem === 'SRO-020: Objeto não encontrado na base de dados dos Correios.') {
-      return 'Objeto não encontrado';
-   } else {
-      return 'Sucesso';
-   }
-}
-
-// Retorna uma etiqueta com o status da entrega
-getStatus = (status) => {
-   try {
-      if (status === "Objeto postado") {
-         return "Obj. Postado";
-      } else if (status === "Objeto entregue ao destinatário") {
-         return "Entregue";
-      } else {
-         return "Em Trânsito";
-      }
-   } catch (err) {
-      console.error(err);
-   }
-}
-
-// Retorna ícones de acordo com o status da entrega
-getImageSrc = (type) => {
-   const path_img = "/images";
-   let img_src = "";
-
-   switch (type) {
-      case "Objeto postado":
-         img_src = `${path_img}/icon-loc1.svg`;
-         break;
-      case "Objeto em trânsito - por favor aguarde":
-         img_src = `${path_img}/icon-truck-transit.svg`;
-         break;
-      case "Objeto recebido pelos Correios do Brasil":
-         img_src = `${path_img}/icon-truck-ok.svg`;
-         break;
-      case "Fiscalização aduaneira finalizada":
-         img_src = `${path_img}/icon-box-search.svg`;
-         break;
-      case "Objeto saiu para entrega ao destinatário":
-         img_src = `${path_img}/icon-hand-box.svg`;
-         break;
-      case "Objeto entregue ao destinatário":
-         img_src = `${path_img}/icon-box-ok.svg`;
-         break;
-      default:
-         img_src = `${path_img}/icon-default.svg`;
-         break;
-   }
-
-   return img_src;
-}
-
-// Formata a data para o formato brasileiro
-dateFormat = (date) => {
-   let aux = "", result = [];
-   aux = date.split("T");
-
-   let new_date = new Date(aux[0]);
-   result.push(new_date.toLocaleDateString('pt-BR', { timeZone: 'UTC' }));
-   result.push(aux[1]);
-
-   return result;
-}
-
-// Armazena o código de rastreio na área de transferência
-copyCode = () => {
-   navigator.clipboard.writeText(this.state.code_track);
-
-   let tooltip = document.getElementById("myTooltip");
-   tooltip.innerHTML = "Copiado!";
-}
-
-// Reseta tooltip de copiar código rastreio
-outFunc = () => {
-   let tooltip = document.getElementById("myTooltip");
-   tooltip.innerHTML = "Copiar Código";
-}
-
-render() {
-   const { isLoading, success, code_track, info, showBtnTop } = this.state;
-
-   let msg_whats = returnMsgWhats(code_track);
-   let msg_email = returnMsgEmail(code_track);
-
-   return (
-      <React.Fragment>
-         <HeadComponent title={"Informações da Entrega - LocPed"} />
-         <NavbarComponent page="info" />
-         {(!isLoading) ?
-            <React.Fragment>
-               {(success === 'Sucesso') ?
-                  <MainComponent>
-                     <div className={styles.header}>
-                        <div className={styles.title}>
-                           <h6 className='mb-0'>cód. rastreio</h6>
-                           <h3 className={styles.code}>
-                              {code_track}
-                              <div className={styles.tooltip}>
-                                 <button className={styles.btn_copy} onClick={this.copyCode} onMouseOut={this.outFunc}>
-                                    <span className={styles.tooltiptext} id="myTooltip">Copiar Código</span>
-                                    <i className="bi bi-files"></i>
-                                 </button>
-                              </div>
-                           </h3>
-                           <StatusPacketComponent status={this.getStatus(info[0].descricao)} />
-                        </div>
-                     </div>
-
-                     <div className={styles.btns_share}>
-                        <p>Compartilhar:</p>
-
-                        <a target="_blank" className="btn btn-primary mx-2" href={msg_email} rel="noopener noreferrer"><i className="bi bi-envelope"></i></a>
-                        <a target="_blank" className={styles.btn_whatsapp} href={msg_whats} rel="noopener noreferrer"><i className="bi bi-whatsapp"></i></a>
-                     </div>
-
-                     <div className={styles.status_section}>
-                        <h5>LINHA DO TEMPO DA ENTREGA</h5>
-                     </div>
-
-                     {(info !== null && info.length > 0) ?
-                        // INFORMAÇÕES SOBRE A ENTREGA
-                        info.map((element, index) => (
-                           <React.Fragment key={index}>
-                              <CardComponent
-                                 src={this.getImageSrc(element.descricao)}
-                                 alt={`ícone ${index}`}
-                                 title={element.descricao}
-                                 date={element.data}
-                                 hours={element.hora}
-                                 place={element.unidade.tipo}
-                                 city={element.unidade.endereco.cidade}
-                                 uf={element.unidade.endereco.uf} />
-                              {(index < info.length - 1) ? <LineStatusComponent /> : ''}
-                           </React.Fragment>
-                        ))
-                        :
-                        // SEM INFORMAÇÕES NO MOMENTO
-                        <React.Fragment>
-                           <div className='text-center'>
-                              <h4>Não há nenhuma informação no momento!</h4>
-                              <div className='my-3'>
-                                 <Image
-                                    src={'/images/location-time.svg'}
-                                    alt={'Sem informações!'}
-                                    width={96}
-                                    height={96}
-                                 // loading='lazy'
-                                 />
-                              </div>
-                              <p>Às vezes as informações demoram até 3 dias úteis para constar no sistema da transportadora. Volte mais tarde!</p>
-                           </div>
-                        </React.Fragment>
-                     }
-
-                     <div className='mt-4'>
-                        <span>Icon by <Link href="https://freeicons.io/profile/5790">ColourCreatype</Link> on <Link href="https://freeicons.io">freeicons.io</Link></span>
-                     </div>
-
-                     {/* BOTÃO SCROLL TO TOP */}
-                     {(showBtnTop === true) ? <ButtonGoTop /> : ""}
-
-                  </MainComponent>
-                  : (success === 'Timeout') ?
-                     <TimeOutComponent />
-                     :
-                     // PÁGINA DE ERRO
-                     <ErrorPageComponent error={success} />
-               }
-            </React.Fragment>
-            :
-            <SpinnerComponent />
+   // Timeout de resposta da api
+   setConnectionTimeout = () => {
+      const timeout = 25;
+      let st = setTimeout(() => {
+         const { isLoading } = this.state;
+         if (isLoading) {
+            this.setState({ success: 'Timeout', isLoading: false });
          }
-         <FooterComponent />
-      </React.Fragment>
-   );
-}
+         clearTimeout(st);
+      }, (timeout * 1000));
+   }
+
+   // Verifica se há informações dos eventos de entrega
+   checkEventos = (data) => ((!data || data === null || data === "") ? null : data);
+
+   // Verifica e formata as informações sobre o endereço onde está o pedido
+   checkAddress = (city, uf) => ((city === undefined || uf === undefined) ? 'Brasil' : `${city}/${uf}`);
+
+   // Verificação de erros na resposta dos dados
+   errorChecking = (response) => {
+      if (response.data.success === false) {
+         return 'Serviço indisponível';
+      }
+
+      if (response.data.result.mensagem === 'SRO-019: Objeto inválido') {
+         return 'Objeto inválido';
+      } else if (response.data.result.mensagem === 'SRO-020: Objeto não encontrado na base de dados dos Correios.') {
+         return 'Objeto não encontrado';
+      } else {
+         return 'Sucesso';
+      }
+   }
+
+   // Retorna uma etiqueta com o status da entrega
+   getStatus = (status) => {
+      try {
+         if (status === "Objeto postado") {
+            return "Obj. Postado";
+         } else if (status === "Objeto entregue ao destinatário") {
+            return "Entregue";
+         } else {
+            return "Em Trânsito";
+         }
+      } catch (err) {
+         console.error(err);
+      }
+   }
+
+   // Armazena o código de rastreio na área de transferência
+   copyCode = () => {
+      navigator.clipboard.writeText(this.state.code_track);
+
+      let tooltip = document.getElementById("myTooltip");
+      tooltip.innerHTML = "Copiado!";
+   }
+
+   // Reseta tooltip de copiar código rastreio
+   outFunc = () => {
+      let tooltip = document.getElementById("myTooltip");
+      tooltip.innerHTML = "Copiar Código";
+   }
+
+   render() {
+      const { isLoading, success, code_track, info, showBtnTop } = this.state;
+
+      let msg_whats = returnMsgWhats(code_track);
+      let msg_email = returnMsgEmail(code_track);
+
+      return (
+         <React.Fragment>
+            <HeadComponent title={"Informações da Entrega - LocPed"} />
+            <NavbarComponent page="info" />
+            {(!isLoading) ?
+               <React.Fragment>
+                  {(success === 'Sucesso') ?
+                     <MainComponent>
+                        <div className={styles.header}>
+                           <div className={styles.title}>
+                              <h6 className='mb-0'>cód. rastreio</h6>
+                              <h3 className={styles.code}>
+                                 {code_track}
+                                 <div className={styles.tooltip}>
+                                    <button className={styles.btn_copy} onClick={this.copyCode} onMouseOut={this.outFunc}>
+                                       <span className={styles.tooltiptext} id="myTooltip">Copiar Código</span>
+                                       <i className="bi bi-files"></i>
+                                    </button>
+                                 </div>
+                              </h3>
+                              <StatusPacketComponent status={this.getStatus(info[0].descricao)} />
+                           </div>
+                        </div>
+
+                        <div className={styles.btns_share}>
+                           <p>Compartilhar:</p>
+
+                           <a target="_blank" className="btn btn-primary mx-2" href={msg_email} rel="noopener noreferrer"><i className="bi bi-envelope"></i></a>
+                           <a target="_blank" className={styles.btn_whatsapp} href={msg_whats} rel="noopener noreferrer"><i className="bi bi-whatsapp"></i></a>
+                        </div>
+
+                        <div className={styles.status_section}>
+                           <h5>LINHA DO TEMPO DA ENTREGA</h5>
+                        </div>
+
+                        {(info !== null && info.length > 0) ?
+                           // INFORMAÇÕES SOBRE A ENTREGA
+                           info.map((element, index) => (
+                              <React.Fragment key={index}>
+                                 <CardComponent
+                                    src={getImageSrc(element.descricao)}
+                                    alt={`ícone ${index}`}
+                                    title={element.descricao}
+                                    date={element.data}
+                                    hours={element.hora}
+                                    place={element.unidade.tipo}
+                                    city={element.unidade.endereco.cidade}
+                                    uf={element.unidade.endereco.uf} />
+                                 {(index < info.length - 1) ? <LineStatusComponent /> : ''}
+                              </React.Fragment>
+                           ))
+                           :
+                           // SEM INFORMAÇÕES NO MOMENTO
+                           <React.Fragment>
+                              <div className='text-center'>
+                                 <h4>Não há nenhuma informação no momento!</h4>
+                                 <div className='my-3'>
+                                    <Image
+                                       src={'/images/location-time.svg'}
+                                       alt={'Sem informações!'}
+                                       width={96}
+                                       height={96}
+                                    // loading='lazy'
+                                    />
+                                 </div>
+                                 <p>Às vezes as informações demoram até 3 dias úteis para constar no sistema da transportadora. Volte mais tarde!</p>
+                              </div>
+                           </React.Fragment>
+                        }
+
+                        <div className='mt-4'>
+                           <span>Icon by <Link href="https://freeicons.io/profile/5790">ColourCreatype</Link> on <Link href="https://freeicons.io">freeicons.io</Link></span>
+                        </div>
+
+                        {/* BOTÃO SCROLL TO TOP */}
+                        {(showBtnTop === true) ? <ButtonGoTop /> : ""}
+
+                     </MainComponent>
+                     : (success === 'Timeout') ?
+                        <TimeOutComponent />
+                        :
+                        // PÁGINA DE ERRO
+                        <ErrorPageComponent error={success} />
+                  }
+               </React.Fragment>
+               :
+               <SpinnerComponent />
+            }
+            <FooterComponent />
+         </React.Fragment>
+      );
+   }
 }
 
 export default TrackInfo;
